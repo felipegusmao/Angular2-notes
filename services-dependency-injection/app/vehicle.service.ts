dde4762981a475d1dfaa8b6af9da36bc9bb84ce0
Felipe Gusmao <fgunix@gmail.com>
2016-03-26 23:49:19 -0300

    Services and dependency injection

---
+++ services-dependency-injection/app/vehicle.service.ts
@@ -0,0 +1,11 @@
+import {Injectable} from 'angular2/core';
+
+@Injectable()
+export class VehicleService {
+  getVehicles = () => [
+    { id: 1, name: 'X-Wing Fighter' },
+    { id: 2, name: 'Tie Fighter' },
+    { id: 3, name: 'Y-Wing Fighter' }
+  ];
+}
+
